f3404ee86fb1cd4b9270924e83f5cc2e7dafa624
Bekich141 <123182789+Bekich141@users.noreply.github.com>
2025-01-17 19:45:40 +0600

    сделано все

--- main.js
+++ main.js
@@ -2,11 +2,7 @@ fetch("./menu.json")
     .then(response => response.json())
     .then(menuData => {
         const contentCold = document.querySelector('.cold__dishes');
-     
-        const spanFilter = document.querySelectorAll('.nav__list-item');
-        console.log(spanFilter);
         const titleCategory = document.querySelector('.cold__title-item');
-        console.log(titleCategory);
 
         const categoryHot = document.querySelector('.hot__content');
         categoryHot.style.display = 'none';
@@ -56,18 +52,34 @@ fetch("./menu.json")
         const coldDishes = allData.filter(item => ["Холодные закуски", "Супы"].includes(item.category));
         renderMenu(coldDishes, contentCold);
 
-        spanFilter.forEach(span => {
-            span.addEventListener('click', () => {
-               
+        const spanFilter = document.querySelectorAll(".nav__list-item")
+        const filterSpan = document.querySelectorAll('.nav__acc');
+        filterSpan[0].classList.add("active-span")
+        filterSpan.forEach((item) => {
+            item.addEventListener("click", () => {
+                event.preventDefault()
+                
+                filterSpan.forEach(item => item.classList.remove("active-span"))
+                item.classList.add("active-span")
+            })
+        })
+        spanFilter.forEach((span) => {
+            let selectadCategory = "Холодные закуски";
+            span.addEventListener('click', (event) => {
+                event.preventDefault()
 
-                const selectadCategory = span.getAttribute('data-category');
+                selectadCategory = span.getAttribute('data-category');
                 console.log("выбрана категория:", selectadCategory);
 
                 titleCategory.textContent = selectadCategory;
                 const filteredData = allData.filter(item => item.category === selectadCategory);
                 renderMenu(filteredData, contentCold)
+
             })
+
+            console.log("выбрана категория:", selectadCategory);
         })
+
     })
     .catch((error) => {
         console.error("Ошибка при получении данных:", error);
